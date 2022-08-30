e35bccca97db33c9369d7cb84869e491ebb21940
Pablo <contactoemail.pablo@gmail.com>
2022-08-30 23:37:58 +0200

    default 1.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Alexandria</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='text-white'>
+      <main className=''>
         <h1 className='text-8xl'>Alexandria</h1>
         <p className='max-w-[50vw] text-2xl pt-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis voluptatibus sapiente iste facilis provident temporibus fuga consequatur corrupti dolore rerum?</p>
       </main>
